3813bad574fb48b9f463522a1f232093246d955f
Piero Pretto <ppretto@amplymedia.com>
2019-10-20 16:58:20 -0500

    one mo again

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(publicPath));
 
