8741ea6ea634135d76a195b536b456182db4e003
Piero Pretto <piero@copilotsearch.com>
2023-04-04 11:04:55 -0500

    new header text

--- src/components/HeaderComponents/Header.js
+++ src/components/HeaderComponents/Header.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 
 const Header= () => (
     <header>
-        <h1>I am a Header Boi</h1>
+        <h1>New Header</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
